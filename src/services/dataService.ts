482e4a6bc823353b9635805b35a01bc2a1d90e06
hoangdh <hoangdh@slitigenz.io>
2025-04-09 17:48:51 +0700

    fix: partner section

--- src/services/dataService.ts
+++ src/services/dataService.ts
@@ -107,7 +107,7 @@ export const fetchVisionPage = async (): Promise<{ data: Vision }> => {
 export const fetchHomePage = async (): Promise<{ data: Home }> => {
   const response = await axiosClient.get(endpoints.getHomepage, {
     params: {
-      populate: ["video", "partners", "partners.logo", "features"],
+      populate: ["video", "features"],
     },
   });
   return response.data;
